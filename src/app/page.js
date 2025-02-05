0a935acf68dcfe9b5f1759abaa9bf6abdf6322f0
mecias <mmnc12@gmail.com>
2025-02-04 23:37:43 -0300

    projeto_barbearia

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,50 @@
+'use client';
+import { useState } from "react";
+import estilos from "./page.module.css";
 import Image from "next/image";
-import styles from "./page.module.css";
+import logo from '../../public/barbearia.png';
+import { BsSun } from "react-icons/bs";
+import { BsMoon } from "react-icons/bs";
+
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+
+  const [alterarFundoPagina, setAlterarFundoPagina] = useState(false);
+
+  function trocaTema() {
+    setAlterarFundoPagina(!alterarFundoPagina);
+  }
+
+  return (
+    <div className={estilos.page}>
+      <main className={alterarFundoPagina ? estilos.main_dark : estilos.main_ligth}>
+        <div className={estilos.container_topo}>
+          <Image className={estilos.img} src={logo} alt="Imagem da logo da barbearia" />
+          <button className={estilos.btn} onClick={trocaTema}>
+            {alterarFundoPagina ? <BsSun className={estilos.icons} /> : <BsMoon />}
+          </button>
+        </div>
+        <div className={estilos.container_banner}>
+
+        </div>
+        <div className={estilos.container_informacao}>
+          <h1>Bem-vindo a Barber Shop</h1>
+
+          <p>
+            Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.
+          </p>
+
+          <p>
+            Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.
+          </p>
+
+          <p className={estilos.paragrafo_diferente}>
+            S. Kelly
+          </p>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
